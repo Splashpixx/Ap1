6c16cb50cdd052a5e63c8d6abc4e8dd9d2d78ac5
Domenic Wolf <53898442+Splashpixx@users.noreply.github.com>
2020-01-20 17:41:40 +0100

    Add files via upload

--- Blatt-4/Aufgabe_5.java
+++ Blatt-4/Aufgabe_5.java
@@ -1,29 +1,35 @@
 import java.math.*;
 import java.util.*;
+import java.awt.geom.*;
 
 class Aufgabe_5 {
 	
-	public static int Dumm( float n, float m ){	
-		return (int) Math.pow(n, m);
+	public static double Dumm( float n, float m ){	
+		return (double) Math.pow(n, m);
 	}
 	
 	public static void main(String[] args) {
-		int[] myIntArray = new int[100];
 			
 			Scanner in = new Scanner(System.in);
 			
 			System.out.println("Programm zur Berechnung der Potenz");
 			System.out.println("Bitte geben Sie x ein:");
-			float x = in.nextFloat();
+			
+			String x = in.next();
 			System.out.println("Bitte geben Sie y ein:");
-			float y = in.nextFloat();
+			String y = in.next();
+			
+			String zwei = x.replace(',', '.');
+			Float zwei2 = new Float(zwei);
+			
+			String hunger = y.replace(',', '.');
+			Float hunger2 = new Float(hunger);
+			
 			
 			System.out.println("Die Potenz von x hoch y ist");
-			float newfloat = Dumm(x, y);
+			double newfloat = Dumm(zwei2, hunger2);
 			String numberAsString = String.valueOf(newfloat);
 
 			System.out.println(numberAsString.replace('.', ','));
-			
- 		
 	}
 }
